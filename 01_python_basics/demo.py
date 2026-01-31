ede6b6c37a101bbf1699c41cf20e41abd35f6ff0
nagaharikaraavi <nagaharikaraavi.nagaharika.raavi@gmail.com>
2026-01-30 19:23:23 -0600

    Basic Python -rules

---
+++ 01_python_basics/demo.py
@@ -0,0 +1,18 @@
+# comments - ignored by interpreter
+# a=10
+# b=20
+# print(a+b)
+
+#print(hello world) # Syntax error:Invalid syntax
+print(9) #number - int
+Print(9.5) # number - Float
+a=10
+Print(a)
+greetings="Good Morning" # text - string
+Print(greetings)
+Print('hello world')
+
+
+#import demo
+import keyword
+print(keyword.kwlist)
