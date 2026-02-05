c0798ba356434675d5d2b5923cfd41cb3b239d87
nagaharikaraavi <nagaharikaraavi.nagaharika.raavi@gmail.com>
2026-02-05 13:40:25 -0600

    Identifiers

--- 01_python_basics/demo.py
+++ 01_python_basics/demo.py
@@ -7,12 +7,38 @@
 print(9) #number - int
 Print(9.5) # number - Float
 a=10
-Print(a)
+print(a)
 greetings="Good Morning" # text - string
-Print(greetings)
-Print('hello world')
+print(greetings)
+print('hello world')
 
 
 #import demo
 import keyword
 print(keyword.kwlist)
+
+
+# Identifiers
+data = "hello"
+# 2daydata = "hello" -> invalid, as it starts with digit
+_2daydata = "hello"
+
+a = 20
+A = 30
+
+print(a)
+print(A)
+
+# class = "some data" -> keywords cannot be identifiers
+# print(class)
+
+# Define Some Student Data
+# Bad way of using identifiers
+x = "Ravi"
+y = 25
+z = "Python Course"
+
+# Good way of using identifiers
+student_firstname = "Ravi"
+student_age = 25
+student_course = "Python Course"
